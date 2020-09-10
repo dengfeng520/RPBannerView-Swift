68b519c9347dd17d8b8c4f8efb4e2e56a42bfba4
dengfeng520 <deng_feng520@163.com>
2020-09-10 09:25:41 +0800

    fix code

---
+++ RPBannerView/RPBannerView.h
@@ -0,0 +1,19 @@
+//
+//  RPBannerView.h
+//  RPBannerView
+//
+//  Created by rp.wang on 2020/9/9.
+//  Copyright © 2020 Beijing Physical Fitness Sport Science and Technology Co.,Ltd. All rights reserved. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RPBannerView.
+FOUNDATION_EXPORT double RPBannerViewVersionNumber;
+
+//! Project version string for RPBannerView.
+FOUNDATION_EXPORT const unsigned char RPBannerViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RPBannerView/PublicHeader.h>
+
+
